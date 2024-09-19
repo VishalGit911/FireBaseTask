4f2b9f66ad5786df8286fee2dd69bd0086fb1a81
VishalGit911 <vishalgit911@gmail.com>
2024-09-19 17:03:39 +0530

    19
    done

--- admin_panel/lib/Views/Home/home_screen.dart
+++ admin_panel/lib/Views/Home/home_screen.dart
@@ -10,6 +10,28 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Grocery Admin Panel"),
+        backgroundColor: Colors.green.shade800,
+        foregroundColor: Colors.white,
+      ),
+      body: GridView.builder(
+        itemCount: 4,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 200,
+              width: 200,
+              color: Colors.blue,
+            ),
+          );
+        },
+      ),
+    );
   }
 }
