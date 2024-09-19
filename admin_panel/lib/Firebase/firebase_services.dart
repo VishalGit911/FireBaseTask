7016ceee6bc7bfc9b89f66954a45a0ee69299a29
VishalGit911 <vishalgit911@gmail.com>
2024-09-19 16:21:00 +0530

    19
    done

--- admin_panel/lib/Firebase/firebase_services.dart
+++ admin_panel/lib/Firebase/firebase_services.dart
@@ -9,11 +9,38 @@ class FirebaseServices {
     return instance;
   }
 
-  Future<UserCredential> gmailPasswordLogin(
-      {required String email, required String password}) async {
-    UserCredential user = await FirebaseAuth.instance
-        .signInWithEmailAndPassword(email: email, password: password);
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-    return user;
+  Future<User?> gmailPasswordLogin(
+      {required String email, required String password}) async {
+    try {
+      final credenial = await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+
+      return credenial.user;
+    } catch (e) {
+      rethrow;
+    }
   }
 }
+
+/*
+  Future<void> register(
+      {required String email, required String password}) async {
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+ */
--- admin_panel/lib/Views/Signin/signin_screen.dart
+++ admin_panel/lib/Views/Signin/signin_screen.dart
@@ -1,5 +1,6 @@
 import 'package:admin_panel/Firebase/firebase_services.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:admin_panel/Views/Home/home_screen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -10,74 +11,147 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final formkey = GlobalKey<FormState>();
+
   TextEditingController emailcontroller = TextEditingController();
   TextEditingController passwordcontroller = TextEditingController();
 
+  void togglechange() {
+    setState(() {
+      isvisibility = !isvisibility;
+    });
+  }
+
+  bool isvisibility = false;
+  bool isloading = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image(image: AssetImage("assets/images/signin.png")),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text(
-                "Get your grocereis\nwith nectar",
-                style: TextStyle(fontSize: 33, fontWeight: FontWeight.bold),
+        child: Form(
+          key: formkey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image(image: AssetImage("assets/images/signin.png")),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(
+                  "Get your grocereis\nwith nectar",
+                  style: TextStyle(fontSize: 33, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 20, right: 20, top: 20, bottom: 5),
-              child: TextFormField(
-                controller: emailcontroller,
-                decoration: InputDecoration(
-                    hintText: "Email",
-                    prefixIcon: Icon(Icons.mail_outline),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15))),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 20, right: 20, top: 20, bottom: 5),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Enter valid email";
+                    } else {
+                      return null;
+                    }
+                  },
+                  controller: emailcontroller,
+                  decoration: InputDecoration(
+                      hintText: "Email",
+                      suffixIcon: Icon(Icons.mail_outline),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15))),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 20, right: 20, top: 20, bottom: 5),
-              child: TextFormField(
-                controller: passwordcontroller,
-                decoration: InputDecoration(
-                    hintText: "Password",
-                    prefixIcon: Icon(Icons.lock),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15))),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 20, right: 20, top: 20, bottom: 5),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Entere valid password";
+                    } else {}
+                  },
+                  controller: passwordcontroller,
+                  decoration: InputDecoration(
+                      hintText: "Password",
+                      suffixIcon: isvisibility
+                          ? IconButton(
+                              onPressed: togglechange,
+                              icon: Icon(Icons.visibility),
+                            )
+                          : IconButton(
+                              onPressed: togglechange,
+                              icon: Icon(Icons.visibility_off)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15))),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 50),
-              child: Center(
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15)),
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.green.shade800,
-                        fixedSize: Size(400, 60)),
-                    onPressed: () async {
-                      UserCredential usercredential = await FirebaseServices()
-                          .gmailPasswordLogin(
-                              email: emailcontroller.text.toString(),
-                              password: passwordcontroller.text.toString());
-                      print(usercredential.user?.email);
-                    },
-                    child: Text(
-                      "SignIn",
-                      style: TextStyle(fontSize: 20),
-                    )),
-              ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 50),
+                child: Center(
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.green.shade800,
+                          fixedSize: Size(400, 60)),
+                      onPressed: () {
+                        signincheck(
+                            email: emailcontroller.text.toString(),
+                            password: passwordcontroller.text.toString());
+                      },
+                      child: isloading
+                          ? CircularProgressIndicator(
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "SignIn",
+                              style: TextStyle(fontSize: 20),
+                            )),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<void> signincheck({required email, required password}) async {
+    if (formkey.currentState!.validate()) {
+      setState(() {
+        isloading = true;
+      });
+
+      try {
+        final user = await FirebaseServices()
+            .gmailPasswordLogin(email: email, password: password);
+
+        if (user != null) {
+          if (kDebugMode) {
+            print(user.email);
+            print(user.uid);
+          }
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(
+              builder: (context) => HomeScreen(),
+            ),
+            (route) => false,
+          );
+          final snackBar = SnackBar(content: Text("Login SuccessFull"));
+
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        }
+      } catch (e) {
+        final snackBar = SnackBar(content: Text("Not user found"));
+
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        print("error : ${e}");
+      } finally {
+        setState(() {
+          isloading = false;
+        });
+      }
+    }
   }
 }
